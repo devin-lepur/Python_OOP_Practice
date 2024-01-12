357dd4aa451841aec3dba937a23a25ddc519201e
Devin Lepur <73671652+devin-lepur@users.noreply.github.com>
2024-01-11 17:31:04 -0800

    Opening and closing file practice

---
+++ practice3.py
@@ -0,0 +1,45 @@
+"""
+    Author: Devin Lepur
+    Opening and writing files in python
+    1/11/24
+    No true goal for the end of this practice, simply familiar with the
+    relevant methods, end program will be very simple/messy
+"""
+
+def readWrite():
+    myFile = open("practice3input.txt", "a+")
+    myFile.write("\nThis is the new last line")
+
+    #Seek is needed to redirect where reading and writing is done
+    #Using "a" access mode defaults it to the end of the file
+    myFile.seek(0)
+    print(myFile.read())
+
+    #Realized seek is characters not lines
+    myFile.seek(5)
+    print(myFile.readline())
+
+    myFile.close()
+
+
+#Makes sense but was surprised to see the newline contained within the line
+def readSpecificLine():
+    myFile = open("practice3input.txt", "r")
+
+    #Reads the contents of myFile into fileContents
+    #Not memory or time efficient but the best way is super complex
+    fileContents = myFile.readlines()
+
+    print(fileContents[5])
+
+def writeNew():
+    myFile = open("practice3output.txt", "w+")
+
+    myFile.write("This is my new file!")
+    
+    myFile.seek(0)
+    print(myFile.read())
+
+readWrite()
+readSpecificLine()
+writeNew()
